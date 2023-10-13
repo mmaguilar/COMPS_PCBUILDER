e922596d5e580209e7525a56690b8aa28018e1e1
Miriam Aguilar <m2002aguilar@gmail.com>
2023-10-12 21:09:51 -0800

    Loading CPU Scene

--- Assets/DropZoneDetection.cs
+++ Assets/DropZoneDetection.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class DropZoneDetection : MonoBehaviour
@@ -8,6 +10,7 @@ public class DropZoneDetection : MonoBehaviour
     public GameObject validAlignmentText;
     public GameObject invalidAlignmentText;
     public XRSocketInteractor socket;
+    public float waitTime;
 
     // Start is called before the first frame update
     private void OnTriggerExit(Collider other)
@@ -15,6 +18,18 @@ public class DropZoneDetection : MonoBehaviour
        invalidAlignmentText.SetActive(false);
        validAlignmentText.SetActive(false);
 
-        print(socket.GetOldestInteractableSelected());
+       print(socket.GetOldestInteractableSelected());
+        if(socket.GetOldestInteractableSelected() != null)
+        {
+            validAlignmentText.SetActive(false);
+            StartCoroutine(LoadCPUScene());
+            SceneManager.LoadScene(2);
+            
+        }
+    }
+
+    IEnumerator LoadCPUScene()
+    {
+        yield return new WaitForSeconds(waitTime);
     }
 }
