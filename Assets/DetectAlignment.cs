4b7686d9799847c228d2ed2c48bc0eff8f787492
Miriam Aguilar <m2002aguilar@gmail.com>
2023-10-12 20:30:37 -0800

    CPU: ALignment

--- Assets/DetectAlignment.cs
+++ Assets/DetectAlignment.cs
@@ -21,6 +21,7 @@ public class DetectAlignment : MonoBehaviour
             validAlignmentText.SetActive(true);
             invalidAlignmentText.SetActive(false);
         }
+       
     }
 
     void OnTriggerStay(Collider other)
--- Assets/DropZoneDetection.cs
+++ Assets/DropZoneDetection.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class DropZoneDetection : MonoBehaviour
 {
     public GameObject validAlignmentText;
     public GameObject invalidAlignmentText;
+    public XRSocketInteractor socket;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        invalidAlignmentText.SetActive(false);
+    }
     // Start is called before the first frame update
     private void OnTriggerExit(Collider other)
     {
-            invalidAlignmentText.SetActive(false);
-            validAlignmentText.SetActive(false);         
+       invalidAlignmentText.SetActive(false);
+       validAlignmentText.SetActive(false);         
     }
 }
