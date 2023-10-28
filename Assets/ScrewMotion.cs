15e4570349a43fd36c8cec3bf70af39d66c81de4
Miriam Aguilar <m2002aguilar@gmail.com>
2023-10-28 13:24:13 -0800

    Cooler: Screw Motion first iteration

--- Assets/ScrewMotion.cs
+++ Assets/ScrewMotion.cs
@@ -14,16 +14,48 @@ public class ScrewMotion : MonoBehaviour
     public float rotateSpeed;
     public float moveSpeed;
 
+    public GameObject previousStopText;
+    public GameObject previousInstructionsText;
+    public GameObject previousPointer;
+
+    public GameObject currentStopText;
+    public GameObject currentInstructionsText;
+    public GameObject currentPointer;
+
+    public GameObject nextStopText;
+    public GameObject nextInstructionsText;
+    public GameObject nextPointer;
+
+    private void Start()
+    {
+        currentStopText.gameObject.SetActive(false);
+        currentInstructionsText.gameObject.SetActive(false);
+        currentPointer.gameObject.SetActive(false);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Checkpoint")
+        {
+            currentStopText.gameObject.SetActive(true);
+            currentInstructionsText.gameObject.SetActive(true);
+            currentPointer.gameObject.SetActive(true);
+        }
+       
+    }
 
     public void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.tag == "Collider")
+        if(previousStopText.gameObject != null && previousInstructionsText.gameObject != null && previousPointer != null)
+        {
+            previousStopText.gameObject.SetActive(false);
+            previousInstructionsText.gameObject.SetActive(false);
+            previousPointer.gameObject.SetActive(false);
+        }
+
+        if (other.gameObject.tag == "Collider")
         {
             StartCoroutine(screwdriverMotion());
-        }
-        if(other.gameObject.tag == "Finished Collider")
-        {
-            StopCoroutine(screwdriverMotion());
         }
     }
 
@@ -31,11 +63,7 @@ public class ScrewMotion : MonoBehaviour
     {
         screwdriverObject.transform.Rotate(0, rotateSpeed, 0);
         boltObject.transform.Translate(0, moveSpeed, 0);
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.01f);
     }
-
-
-
-
 
 }
