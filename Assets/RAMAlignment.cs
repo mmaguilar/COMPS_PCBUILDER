e3478db5eac0a58039b72352d39cc8ecdc110f3f
Miriam Aguilar <m2002aguilar@gmail.com>
2023-11-02 16:24:48 -0800

    RAM: Finalizing scene

--- Assets/RAMAlignment.cs
+++ Assets/RAMAlignment.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class RAMAlignment : MonoBehaviour
 {
-    private void OnTriggerStay(Collider other)
+    public GameObject validAlignmentText;
+    public GameObject InvalidAlignmentText;
+
+    public  void Start()
+    {
+        validAlignmentText.SetActive(false);
+        InvalidAlignmentText.SetActive(false);
+    }
+    public void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag == "Collider")
         {
+            validAlignmentText.SetActive(true);
+            if (InvalidAlignmentText.activeSelf)
+            {
+                InvalidAlignmentText.SetActive(false);
+            }
             print("Aligned");
         }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        InvalidAlignmentText.SetActive(true);
+        validAlignmentText.SetActive(false);
     }
 }
