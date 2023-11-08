0693d9f708118c902aa3bdd19bc427babd244f2e
Miriam Aguilar <m2002aguilar@gmail.com>
2023-11-07 16:54:26 -0800

    Finishing Mobo Install and Starting GPU Scene

---
+++ Assets/MoboVerifyInstall.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoboVerifyInstall : MonoBehaviour
+{
+    public GameObject completedText1;
+    public GameObject completedText2;
+    public GameObject completedText3;
+    public GameObject completedText4;
+    public GameObject completedText5;
+    public GameObject completedText6;
+
+    public GameObject completedInstallText;
+
+    public FadeScreen fadeScreen;
+    public int SceneToTransition;
+
+
+    private void Start()
+    {
+        completedInstallText.SetActive(false);
+    }
+    private void Update()
+    {
+        if(completedText1.activeSelf && completedText2.activeSelf && completedText3.activeSelf
+            && completedText4.activeSelf && completedText5.activeSelf && completedText6.activeSelf)
+        {
+            print("Install complete");
+            completedText1.SetActive(false);
+            completedText2.SetActive(false);
+            completedText3.SetActive(false);
+            completedText4.SetActive(false);
+            completedText5.SetActive(false);
+            completedText6.SetActive(false);
+            completedInstallText.SetActive(true);
+            GoToScene(SceneToTransition);
+        } 
+    }
+
+    public void GoToScene(int scene)
+    {
+        StartCoroutine(GoToSceneRoutine(scene));
+    }
+
+    IEnumerator GoToSceneRoutine(int scene)
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+
+        //Launch the new Scene 
+        SceneManager.LoadScene(scene);
+    }
+
+}
