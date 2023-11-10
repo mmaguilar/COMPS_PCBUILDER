d92c59c77427709d24ded6df9a66e34af20046e5
Miriam Aguilar <m2002aguilar@gmail.com>
2023-11-09 20:16:34 -0800

    Major changes

---
+++ Assets/PowerSupplyAlignment.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerSupplyAlignment : MonoBehaviour
+{
+    public GameObject validAlignmentText;
+    public GameObject InvalidAlignmentText;
+    public void Start()
+    {
+        validAlignmentText.SetActive(false);
+        InvalidAlignmentText.SetActive(false);
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Collider")
+        {
+            validAlignmentText.SetActive(true);
+            if (InvalidAlignmentText.activeSelf)
+            {
+                InvalidAlignmentText.SetActive(false);
+            }
+            print("Aligned");
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        InvalidAlignmentText.SetActive(true);
+        validAlignmentText.SetActive(false);
+    }
+}
